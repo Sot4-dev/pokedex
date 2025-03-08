656c929b16f7c769122e541b24f731b6473ead58
Sot4 <Sot4.develop@gmail.com>
2025-03-08 03:31:23 +0000

    idを入力するとpokeAPIからデータを受け取って表示する機能を実装した

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import requests
 import json
+
+pokemon_id = int(input())
+response=requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/")
+if response.status_code==200:
+    pokemon_data=response.json()
+    name=pokemon_data['name']
+    height=pokemon_data['height']/10
+    weight=pokemon_data['weight']/10
+    print(f"名前:{name}")
+    print(f"高さ:{height}m")
+    print(f"重さ:{weight}kg")
+else:
+    print("error")
